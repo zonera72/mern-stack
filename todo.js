e9533c35a60b4b71b775783c9761497f6074487e
zonera72 <37996812+zonera72@users.noreply.github.com>
2020-11-09 19:03:46 +0500

    new changes

--- todo.js
+++ todo.js
@@ -1,16 +1,45 @@
-window.onload=function(){
-    var addItemBtn = document.querySelector(".addItem");
-    
-    addItemBtn.addEventListener("click", function(){
-      
-        var list = document.querySelector("#list");
-        var itemValue= document.querySelector("#newItem").value;
-        var item =document.createElement("li");
-        item.appendChild(document.createTextNode(itemValue));
-        list.appendChild(item);
+/* 
+JAVSCRIPT CODE
 
-        item.addEventListener("click", function(){
-            item.classList.add("done");
-      });
-    }); 
-};
+// window.onload=function(){
+//     var addItemBtn = document.querySelector(".addItem");
+    
+//     addItemBtn.addEventListener("click", function(){
+      
+//         var list = document.querySelector("#list");
+//         var itemValue= document.querySelector("#newItem").value;
+//         var item =document.createElement("li");
+//         item.appendChild(document.createTextNode(itemValue));
+//         list.appendChild(item);
+
+//         item.addEventListener("click", function(){
+//             item.classList.add("done");
+//       });
+//     }); 
+// };
+
+*/
+
+/* JQUERY CODE 
+*/
+
+$(function() {
+    $(".addItem").click(function(){
+        var itemValue= $("#newItem").val();
+        if(!itemValue){
+            $("#newItem").addClass("error");
+            return;
+        }
+        else{
+            $("#newItem").removeClass("error");
+        }
+        var item = "<li>"+itemValue+"</li>";
+        $("#list").append(item);
+
+        $("li").click(function(){
+          $(this).addClass("done");
+                 });
+    });
+
+    
+});
